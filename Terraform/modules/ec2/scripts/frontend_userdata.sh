decfb1a018b314a407f95a28fb0a5b9d1937149f
Ubuntu <ubuntu@ip-172-31-43-116.ec2.internal>
2024-10-30 06:50:07 +0000

    updates

--- Terraform/modules/ec2/scripts/frontend_userdata.sh
+++ Terraform/modules/ec2/scripts/frontend_userdata.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo ${ssh_key} > /home/ubuntu/.ssh/uzo_wl5.pem 
+echo "${ssh_key}" > /home/ubuntu/.ssh/uzo_wl5.pem 
 kura_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDSkMc19m28614Rb3sGEXQUN+hk4xGiufU9NYbVXWGVrF1bq6dEnAD/VtwM6kDc8DnmYD7GJQVvXlDzvlWxdpBaJEzKziJ+PPzNVMPgPhd01cBWPv82+/Wu6MNKWZmi74TpgV3kktvfBecMl+jpSUMnwApdA8Tgy8eB0qELElFBu6cRz+f6Bo06GURXP6eAUbxjteaq3Jy8mV25AMnIrNziSyQ7JOUJ/CEvvOYkLFMWCF6eas8bCQ5SpF6wHoYo/iavMP4ChZaXF754OJ5jEIwhuMetBFXfnHmwkrEIInaF3APIBBCQWL5RC4sJA36yljZCGtzOi5Y2jq81GbnBXN3Dsjvo5h9ZblG4uWfEzA2Uyn0OQNDcrecH3liIpowtGAoq8NUQf89gGwuOvRzzILkeXQ8DKHtWBee5Oi/z7j9DGfv7hTjDBQkh28LbSu9RdtPRwcCweHwTLp4X3CYLwqsxrIP8tlGmrVoZZDhMfyy/bGslZp5Bod2wnOMlvGktkHs="
-echo $kura_key >> /home/ubuntu/.ssh/authorized_keys
+echo "$kura_key" >> /home/ubuntu/.ssh/authorized_keys
 
 
 # Update system
